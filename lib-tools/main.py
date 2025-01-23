9f81f960c28ed9635ffeb980e9aea836dc5b98ca
Aditya Gupta <mail@adityagupta.dev>
2025-01-23 22:37:50 +0530

    feat: tag delimeter fix

---
+++ lib-tools/main.py
@@ -0,0 +1,57 @@
+from mutagen import File
+import os
+
+def fix_tags(file_path) -> None:
+    """
+    Correctly splits tags that are just delimited by a comma or semi colon
+    """
+
+    audio = File(file_path)
+
+    if not audio:
+        raise ValueError("File is not an audio file.")
+    
+    if not audio.tags:
+        raise AttributeError("Music file does not have any tags??")
+    
+    artists = audio.tags.get("artist") or audio.tags.get("ARTIST")
+    genres = audio.tags.get("genre") or audio.tags.get("GENRE")
+    
+    artists = split_tag(artists)
+    genres = split_tag(genres)
+
+    if artists:
+        print("Fixing artists tag for ", audio["TITLE"])
+        audio["ARTIST"] = artists
+    if genres:
+        print("Fixing genres tag for ", audio["TITLE"])
+        audio["GENRE"] = genres
+    audio.save()
+
+
+def split_tag(tag) -> list[str]:
+    if tag is None:
+        return
+    
+    if isinstance(tag, list):
+        if len(tag) > 1:
+            return
+        tag_str = tag[0]
+    else:
+        tag_str = tag
+
+    if ";" in tag_str:
+        print("Changing delimeter from ;")
+        return [x.strip() for x in tag_str.split(";")]
+    if "," in tag_str:
+       print("Changing delimeter from ,")
+       return [x.strip() for x in tag_str.split(",")] 
+    else:
+        return None
+
+if __name__ == "__main__":
+    directory = input("Enter music library: ")
+    for root, _, files in os.walk(directory):
+        for file in files:
+            fix_tags(os.path.join(root, file))
+    
